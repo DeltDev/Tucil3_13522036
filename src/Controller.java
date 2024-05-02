0d4c82af0cfcfd02e57161974074ce73040cd978
DeltDev <deltenc88@gmail.com>
2024-05-02 20:11:58 +0700

    feat: word list scene and scene updates

--- src/Controller.java
+++ src/Controller.java
@@ -38,4 +38,13 @@ public class Controller {
         stage.show();
         System.out.println("Mulai");
     }
+
+    public void GoToWordListScene(ActionEvent e) throws IOException{ //controller tombol mulai di scene utama
+        root = FXMLLoader.load(getClass().getResource("wordlistscene.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("Mulai");
+    }
 }
