24c3c3905e66bdcc61172edefa9bf5a3627280d6
DeltDev <deltenc88@gmail.com>
2024-05-02 12:03:01 +0700

    feat: change scene

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,24 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 public class Controller {
     @FXML
-
-    public void Mulai(ActionEvent e){ //controller tombol mulai di scene utama
-        
+    private Scene scene;
+    private Stage stage;
+    private Parent root;
+    public void Mulai(ActionEvent e) throws IOException{ //controller tombol mulai di scene utama
+        root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
         System.out.println("Mulai");
     }
     
--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
   public void start(Stage stage) {
 
     try{
-        Parent root = FXMLLoader.load(getClass().getResource("scenetest.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("landscene.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
