48b9753055fc8d53c6e38365ce31997f1d98f54c
DeltDev <deltenc88@gmail.com>
2024-05-03 15:29:59 +0700

    feat: text input handling and make input case insensitive

--- src/MainSceneController.java
+++ src/MainSceneController.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -12,6 +13,8 @@ import javafx.scene.Node;
 import java.net.URL;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import java.io.FileReader;
+
 public class MainSceneController implements Initializable {
     private Scene scene;
     private Stage stage;
@@ -35,15 +38,19 @@ public class MainSceneController implements Initializable {
     
     public void GoToResultScene(ActionEvent e) throws IOException{ //controller tombol mulai di scene utama
         String chosenMethod = MethodChoiceBox.getValue();
-        String startingWord = StartingWord.getText();
-        String endingWord = EndingWord.getText();
+        String startingWord = StartingWord.getText().toLowerCase();
+        String endingWord = EndingWord.getText().toLowerCase();
+        System.out.println(startingWord);
+        System.out.println(endingWord);
         if(StartingWord.getText().isEmpty()|| EndingWord.getText().isEmpty()){
             ErrorMessage.setText("Anda belum menginput kata! Silakan menginput kata.");
         } else if(startingWord.length() != endingWord.length()){
             ErrorMessage.setText("Panjang kata awal dan akhir berbeda! Harap input dua kata yang panjangnya sama.");
         } else if(chosenMethod == null){
             ErrorMessage.setText("Anda belum memilih metode pencarian! Silakan pilih metode pencarian");
-        } else {
+        } else if(!ValidateWord(startingWord, startingWord.length()) || !ValidateWord(endingWord, endingWord.length())){
+            ErrorMessage.setText("Kata yang dimasukkan tidak ada di dalam kamus/daftar kata program ini!\nSilakan buka daftar kata untuk melihat daftar kata yang valid.");
+        }else {
             System.out.println(chosenMethod);
             execTime = 6969;
             nodesVisited = 420;
@@ -74,5 +81,25 @@ public class MainSceneController implements Initializable {
     public void initialize(URL arg1, ResourceBundle arg2){
         MethodChoiceBox.getItems().addAll(MethodList);
         ErrorMessage.setText("");
+    }
+
+    public boolean ValidateWord(String check, int len){ //periksa apakah kata ada di dalam dictionary
+        
+        try (BufferedReader BR = new BufferedReader(new FileReader("./src/dictionary/dictionary.txt"))){
+            String line;
+            while ((line = BR.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty()) {
+                    continue;
+                }
+                if(line.equals(check)){
+                    return true;
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
